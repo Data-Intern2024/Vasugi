9134d84548078180b9691c7da1e696f7db138724
Data-Intern2024 <Iswariya@eminds.ai>
2024-08-05 18:26:51 +0530

    Update Linear_search.java

--- Linear_search.java
+++ Linear_search.java
@@ -1,16 +1,41 @@
+/*Linear search */
+import java.util.Scanner;
+class Linearsearch{
+    public static int search(int arr[], int x){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of value");
+        int n = sc.nextInt();
 
-class LinearSearch{    
-public static int linearSearch(int[] arr, int key){    
-        for(int i=0;i<arr.length;i++){    
-            if(arr[i] == key){    
-                return i;    
-            }    
-        }    
-        return -1;    
-    }    
-    public static void main(String a[]){    
-        int[] a1= {10,20,30,50,70,90};    
-        int key = 70;    
-        System.out.println(key+" is found at index: "+linearSearch(a1, key));    
-    }    
-}
+        int arr[] = new int[n];
+        System.out.println("Enter the values:");
+
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value:");
+        int x =sc.nextInt();
+        
+        int result = search(arr,x);
+        if(result != -1){
+            System.out.println("Value " + x + " found at " + result);
+        }
+        else{
+            System.out.println("Value " + x + " not found  at index");
+
+        }
+        }
+
+    }
+
+
+
+
+
